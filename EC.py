468db12a7136490b930d43e4aef7d2cffc4042a1
Eternal-Chaos <33375134+Eternal-Chaos@users.noreply.github.com>
2017-11-05 08:35:51 +0500

    Update EC.py

--- EC.py
+++ EC.py
@@ -9,7 +9,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence( game=discord.Game( name="EC TESTSU BOT ( ' - ')7",type = 1 ))
+    await client.change_presence( game=discord.Game( name="EC TESTSU BOT (   ' - ')7",type = 1 ))
 
 @client.event
 async def on_message(message):
@@ -27,5 +27,6 @@ async def on_message(message):
      
     elif message.content.startwith('*addemoji'):
         await client.create_custom_emoji(message.server, "name", image = "x.png")
+        await client.send_message(message.channel, 'Emoji has been added!')
 
 client.run('Mzc1MTM4OTg5Mzk4Njg3NzQ2.DN9l9w.wOUvAFqhJHBJs8NOpHgaBsHFpzY')
