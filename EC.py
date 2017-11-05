2106b4f76ad1d72314f5244f85fc3f616bff0466
Eternal-Chaos <33375134+Eternal-Chaos@users.noreply.github.com>
2017-11-05 10:59:47 +0500

    Update EC.py

--- EC.py
+++ EC.py
@@ -9,16 +9,23 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence( game=discord.Game( name="EC TESTSU BOT (   ' - ')7",type = 1 ))
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('*addemoji'):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+   
+    elif message.content.startswith('*addemoji'):
         await client.create_custom_emoji(message.server, "name", image = "x.png")
         await client.send_message(message.channel, 'Emoji has been added!')
         
-    elif message.content.startswith('*sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-
 client.run('Mzc1MTM4OTg5Mzk4Njg3NzQ2.DN9l9w.wOUvAFqhJHBJs8NOpHgaBsHFpzY')
