279438cc1187d12c084c2479ed1a33c7241b64ec
Eternal-Chaos <33375134+Eternal-Chaos@users.noreply.github.com>
2017-11-05 10:45:30 +0500

    Update EC.py

--- EC.py
+++ EC.py
@@ -13,20 +13,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('*test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    if message.content.startwith('*addemoji'):
+        await client.create_custom_emoji(message.server, "name", image = "x.png")
+        await client.send_message(message.channel, 'Emoji has been added!')
+        
     elif message.content.startswith('*sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
-     
-    elif message.content.startwith('*addemoji'):
-        await client.create_custom_emoji(message.server, "name", image = "x.png")
-        await client.send_message(message.channel, 'Emoji has been added!')
 
 client.run('Mzc1MTM4OTg5Mzk4Njg3NzQ2.DN9l9w.wOUvAFqhJHBJs8NOpHgaBsHFpzY')
