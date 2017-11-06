8aad1bc5e86e51861482e527786abfdbadbc8b02
Eternal-Chaos <33375134+Eternal-Chaos@users.noreply.github.com>
2017-11-06 14:32:14 +0500

    Update EC.py

--- EC.py
+++ EC.py
@@ -4,8 +4,7 @@ from discord.ext import commands
 
 bot = commands.Bot(
     command_prefix = "*",
-    description = "I'm a simple man. I see a command, I call it."
-)
+    description = "I'm a simple man. I see a command, I call it.")
 
 session = ClientSession(loop = bot.loop)
 
