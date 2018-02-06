704d1e2b5631304ee4c7960ec7df6ebe83c9475e
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-06 12:24:13 +0500

    :D

--- cogs/emojis.py
+++ cogs/emojis.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 from discord.ext import commands
+import aiohttp
 
 class emojis:
     def __init__(self, bot):
@@ -11,7 +12,16 @@ class emojis:
     async def addemoji(ctx, emoji_name, emoji_link = ''):
         msg: discord.Message = ctx.message
         if msg.attachments:
-          await ctx.message.delete()
+            image = msg.attachments[0]
+        elif emoji_link:
+            async with session.get(emoji_link) as resp:
+                image = await resp.read()
+        else:
+            await bot.say("No valid emoji provided.")
+            return
+    
+        created_emoji = await bot.create_custom_emoji(ctx.server, name = emoji_name, image = image)
+        await bot.say("Emoji {} created!".format(created_emoji))
           
 def setup(bot):
     bot.add_cog(emojis(bot))
