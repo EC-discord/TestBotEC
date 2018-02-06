0ec5bb07c218dd2356cb26a07f1f3e5bcf46ac3c
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-06 13:03:06 +0500

    .-.

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -1,10 +1,50 @@
 import discord
-import asyncio
 from discord.ext import commands
+from discord.ext.commands import TextChannelConverter
+from contextlib import redirect_stdout
+from ext.utility import load_json
+from urllib.parse import quote as uriquote
+from lxml import etree
+from ext import fuzzy
+from ext import embedtobox
+from PIL import Image
+import unicodedata
+import traceback
+import textwrap
+import aiohttp
+import inspect
+import asyncio
+import time
+import re
+import io
+import os
+import random
+from mtranslate import translate
+from io import BytesIO
+import string
 
 class Misc: 
      def __init__(self, bot):
          self.bot = bot
+     
+     def getColor(self, colorHex):
+        return discord.Colour(int(f'0x{colorHex}', 16))
+
+     def randomcolor(self):
+       color = ''.join([random.choice(string.hexdigits) for _ in range(6)])
+       return self.getColor(color)  
+
+     @commands.command()
+     async def rc(self, ctx):
+        '''Generates a random color'''
+        file = io.BytesIO()
+        color = self.randomcolor()
+        Image.new('RGB', (200, 90), color.to_rgb()).save(file, format='PNG')
+        file.seek(0)
+        em = discord.Embed(color=color, title=f'Showing Color: {str(color)}')
+        em.set_image(url='attachment://color.png')
+        await ctx.send(file=discord.File(file, 'color.png'), embed=em)
+
      
      @commands.command()
      async def getchanid(self, ctx):
