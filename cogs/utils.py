0b86c21fdcb4b5de9acce92e06588b708cba8f66
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-08 12:18:24 +0500

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -186,7 +186,7 @@ class Utility:
         color = await ctx.get_dominant_color(ctx.author.avatar_url)
 
         for embed in pages:
-            embed = discord.Embed(color=0xffd500)
+            em.color = discord.Color(color)
             try:
                 await ctx.send(embed=embed)
             except discord.HTTPException:
