f35a6aeabd15f083041c1ff72f4eadc2a0b8f9fe
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-08 16:24:16 +0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -11,7 +11,7 @@ class general:
     def __init__(self, bot):
         self.bot = bot
 	
-	@commands.command()
+    @commands.command()
     async def clean(self, ctx, limit : int=10000):
         '''Clean a number of the bots messages'''
         await ctx.purge(limit=limit+1, check=lambda m: m.author == ctx.author)
