a7f09acd0aa1fa6c8ba5caac0b6027acd16dcd3f
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-16 22:20:10 +0500

    Update Bitwise.py

--- cogs/Bitwise.py
+++ cogs/Bitwise.py
@@ -16,3 +16,10 @@ class BitwiseOperators:
         '''**Bitwise OR** Each bit of the output is 0 if the corresponding bit of x AND of y is 0, otherwise it's 1'''
         result = x | y
         await ctx.send(result)
+        
+    @commands.command()
+    async def bitxor(self, ctx, *, x:int, y:int):
+        '''**Bitwise XOR** Each bit of the output is the same as the corresponding bit in x if that bit in y is 0, and it's the complement of the bit in x if that bit in y is 1'''
+        result = x ^ y
+        await ctx.send(result)
+        
