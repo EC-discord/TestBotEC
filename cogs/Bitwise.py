8f4e0601a3a52b9c1ef949bab04a61645ce6fe86
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-16 22:28:37 +0500

    Update Bitwise.py

--- cogs/Bitwise.py
+++ cogs/Bitwise.py
@@ -25,7 +25,12 @@ class BitwiseOperators:
         
     @commands.command()
     async def bitlshift(self, ctx, *, x:int, y:int):
-         '''**Left Shift** Returns x with the bits shifted to the left by y places (and new bits on the right-hand-side are zeros). This is the same as multiplying x by 2**y'''
+         '''**Left Shifts Bits** Returns x with the bits shifted to the left by y places (and new bits on the right-hand-side are zeros). This is the same as multiplying x by 2**y'''
          result = x << y
           await ctx.send(result)
-        
+          
+    @commands.command()
+    async def bitrshift(self, ctx, *, x:int, y:int):
+          '''**Right Shifts Bits** Returns x with the bits shifted to the right by y places. This is the same as //'ing x by 2**y'''
+          result = x >> y
+          await ctx.send(result)
