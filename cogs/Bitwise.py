6d7073767e575162011e41c5c500e7d62481a79b
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-17 11:31:18 +0500

    Update Bitwise.py

--- cogs/Bitwise.py
+++ cogs/Bitwise.py
@@ -13,31 +13,31 @@ class BitwiseOperators:
         await ctx.send(result)
         
     @commands.command()
-    async def bitor(self, ctx, *, x:int, y:int):
+    async def bitor(self, ctx, x:int, y:int):
         '''**Bitwise OR** Each bit of the output is 0 if the corresponding bit of x AND of y is 0, otherwise it's 1'''
         result = x | y
         await ctx.send(result)
         
     @commands.command()
-    async def bitxor(self, ctx, *, x:int, y:int):
+    async def bitxor(self, ctx, x:int, y:int):
         '''**Bitwise XOR** Each bit of the output is the same as the corresponding bit in x if that bit in y is 0, and it's the complement of the bit in x if that bit in y is 1'''
         result = x ^ y
         await ctx.send(result)
         
     @commands.command()
-    async def bitlshift(self, ctx, *, x:int, y:int):
+    async def bitlshift(self, ctx, x:int, y:int):
          '''**Left Shifts Bits** Returns x with the bits shifted to the left by y places (and new bits on the right-hand-side are zeros). This is the same as multiplying x by 2**y'''
          result = x << y
          await ctx.send(result)
           
     @commands.command()
-    async def bitrshift(self, ctx, *, x:int, y:int):
+    async def bitrshift(self, ctx, x:int, y:int):
           '''**Right Shifts Bits** Returns x with the bits shifted to the right by y places. This is the same as //'ing x by 2**y'''
           result = x >> y
           await ctx.send(result)
             
     @commands.command()
-    async def bitnot(self, ctx, *, x:int):
+    async def bitnot(self, ctx, x:int):
           '''**Bitwise NOT** Returns the complement of x - the number you get by switching each 1 for a 0 and each 0 for a 1. This is the same as -x - 1'''
           result = ~x
           await ctx.send(result)
