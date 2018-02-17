343b3e8d80c08b17e284a2f3f1971bc2d725d27b
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-17 13:22:09 +0500

    Update Bitwise.py

--- cogs/Bitwise.py
+++ cogs/Bitwise.py
@@ -10,7 +10,7 @@ class BitwiseOperators:
     async def bitand(self, ctx, x:int, y:int):
         '''Bitwise AND : Each bit of the output is 1 if the corresponding bit of x AND of y is 1, otherwise it's 0'''
         result = x & y
-        await ctx.send("**Base 10** : " + result)
+        await ctx.send("**Base 10** : %s" % result)
         
     @commands.command()
     async def bitor(self, ctx, x:int, y:int):
