5483d57e3d1ce98b3f2060af060911638df6bc53
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-20 17:11:08 +0500

    Update Bitwise.py

--- cogs/Bitwise.py
+++ cogs/Bitwise.py
@@ -14,10 +14,10 @@ class BitwiseOperators:
         kappa2 = bin(y)[2:]
         result2 = bin(result)[2:]
         if kappa > kappa2:
-            diff = len(f'{bin(kappa - kappa2)[2:]}')
+            diff = len(f'{int(kappa) - int(kappa2)}')
             kappa = kappa.rjust(diff, '0')
         elif kappa2 > kappa:
-            diff = len(f'{bin(kappa2 - kappa)[2:]}')
+            diff = len(f'{int(kappa2) - int(kappa)}')
             kappa2 = kappa2.rjust(diff, '0')
         await ctx.send("```%s\n%s\n----------------\n%s```" % (kappa, kappa2, result2))
         await asyncio.sleep(0.5)
