f6fc0f1dd07346e40fac56e0c8fb406a697114e4
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-03-06 19:18:04 +0500

    Update modutil.py

--- cogs/modutil.py
+++ cogs/modutil.py
@@ -137,7 +137,7 @@ class Mod:
     @commands.command()
     async def clean(self, ctx, limit: int):
         '''Clean the bots messages'''
-        print(limit)
+        print(typeof(limit))
         await ctx.purge(limit = limit + 1, check = lambda message: message.author.id == 37513898939868774)
 
 
