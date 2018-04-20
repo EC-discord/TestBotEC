042e14914f5432aa4603de931cbac4b8659ee1bd
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-04-20 22:37:10 +0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -61,8 +61,6 @@ class general:
              servers.append(allemojis)
          for server in servers:
              emo = '''\n'''.join(server)
-         var = emo.split('\n')
-         print(var)
          for stuff in var:
              paginator.addLine(stuff)
          for page in paginator.pages:
