e35c31d67fc6d607969c3e16ac70bd80a5568c16
EC <saeqaed@yahoo.com>
2018-07-15 13:50:32 +0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -17,9 +17,9 @@ class general:
     @commands.command()
     async def invite(self,ctx):
         '''invite the bot :D'''
-        em = discord.Embed(color = 0xffd500, title = "Click Here To Invite", url = "https://discordapp.com/oauth2/authorize?client_id=375138989398687746&scope=bot&permissions=305196166")
+        em = discord.Embed(color = 0xffd500, title = "Click Here To Invite", url = "https://discordapp.com/oauth2/authorize?client_id=467973617536335872&scope=bot&permissions=305196166")
         em.set_thumbnail(url = "https://cdn.discordapp.com/attachments/274387797140570112/409323858437472257/image.jpg")
-        em.add_field(name = "Jake The Bot", value = """**Support Server** \nServer Link: https://discord.gg/bmeBBdd """, inline = True)
+        em.add_field(name = "Jake The Bot", value = "A bot made by EC", inline = True)
         em.set_footer(text = "Jake",icon_url = self.bot.user.avatar_url_as(static_format='png'))
         await ctx.send(embed = em)
 
