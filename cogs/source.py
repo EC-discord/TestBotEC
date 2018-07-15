be15d9a7e153b31e191a15711e9a75f1c58142af
EC <saeqaed@yahoo.com>
2018-07-15 22:08:07 +0500

    Create source.py

---
+++ cogs/source.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import inspect
+
+class source:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def source(self, ctx, *, text: str):
+        """Shows source code of a command."""
+        nl2 = '`'
+        nl = f"``{nl2}"
+        source_thing = inspect.getsource(self.bot.get_command(text).callback)
+        await ctx.send(f"{nl}py\n{source_thing}{nl}")
+        
+def setup(bot):
+    bot.add_cog(source(bot))
