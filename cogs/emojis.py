88ea719b1af32365db8c3da8720b361c0ea40470
EC <saeqaed@yahoo.com>
2018-09-21 15:26:33 +0500

    Update emojis.py

--- cogs/emojis.py
+++ cogs/emojis.py
@@ -27,9 +27,9 @@ class emojis:
         
     @commands.command()
     async def getemoji(self, ctx, emoji_name):
-        for guilds in self.bot.emojis:
-            emoji = discord.utils.get(guilds.emojis, name=emoji_name)
-        await ctx.send(emoji)
+        for emoji in self.bot.emojis:
+          if emoji_name == emoji.name:
+            await ctx.send(emoji)
     
     @commands.command()
     async def emojiurl(self, ctx, emoji: discord.Emoji):
