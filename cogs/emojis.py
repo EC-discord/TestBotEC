6773e9edcf450ea45a6d6e10d210d2defbc4e35a
EC <saeqaed@yahoo.com>
2019-03-08 14:01:45 +0500

    Update emojis.py

--- cogs/emojis.py
+++ cogs/emojis.py
@@ -7,23 +7,24 @@ class emojis(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-
     @commands.command()
-    async def addemoji(self, ctx, emoji_name, emoji_link = ''):
-        msg: discord.Message = ctx.message
-        if ctx.author.guild_permissions.manage_emojis == True:
-            if msg.attachments:
-                image = msg.attachments[0]
-            elif emoji_link:
-                async with ctx.session.get(emoji_link) as resp:
-                    image = await resp.read()
-            else:
-                await ctx.send("No valid emoji provided.")
-                return
-            created_emoji = await ctx.guild.create_custom_emoji(name = emoji_name, image = image)
-            await ctx.send("Emoji {} created!".format(created_emoji))
-        else:
-            await ctx.send("You do not have the **Manage emojis** perm")
+    @commands.has_permissions(manage_emojis = True)
+    async def addemoji(self, ctx, emoji_name, emoji_link = '', *roles : discord.Role):
+        """Add an emoji to a server
+        __**Parameters**__
+        • emoji_name – The emoji name. Must be at least 2 characters
+        • emoji_link – The url or attachment of an image to turn into an emoji
+        • roles – A list of Roles that can use this emoji (case sensitive). Leave empty to make it available to everyone
+        """
+        if ctx.message.attachments:
+            emoji_link = ctx.message.attachments[0].url
+            async with ctx.session.get(emoji_link) as resp:
+                image = await resp.read()
+        elif emoji_link:
+            async with ctx.session.get(emoji_link) as resp:
+                image = await resp.read()
+        created_emoji = await ctx.guild.create_custom_emoji(name = emoji_name, image = image, roles = [r for r in roles if roles is not None])
+        await ctx.send(f"Emoji {created_emoji} created!")
         
     @commands.command()
     async def getemoji(self, ctx, *, emoji : discord.Emoji):
