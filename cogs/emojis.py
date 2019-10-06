13a24e9eb402ba43e3e7967bfa8f91fb2e27797b
EC <saeqaed@yahoo.com>
2019-10-06 11:35:18 +0500

    Update emojis.py

--- cogs/emojis.py
+++ cogs/emojis.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 from aiohttp import ClientSession
 from discord import Webhook, RequestsWebhookAdapter
 import requests
-import io
 import aiohttp
 
 class emojis(commands.Cog):
@@ -33,12 +32,9 @@ class emojis(commands.Cog):
     @commands.command(aliases = ["ge"])
     async def getemoji(self, ctx, *, emoji : discord.Emoji):
         if emoji in ctx.guild.emojis:
-            async with ctx.session.get(str(emoji.url)) as resp:
-                image = await resp.read()
             async with aiohttp.ClientSession() as session:
                 webhook = Webhook.partial(630286272170164234, 'frxt9_VGztLr4N5qiZl8akrx_UFckH4c7dg1Ulw9RlFDcsDRd4tLc1bRcDTgP-RQTjNm', adapter=RequestsWebhookAdapter())
-                with io.BytesIO(image) as file:
-                    webhook.send(emoji, username=ctx.message.author.nick or ctx.message.author.name, avatar=file)
+                webhook.send(emoji, username=ctx.message.author.nick or ctx.message.author.name, avatar_url=ctx.message.author.avatar_url_as(format="png"))
         else:
             await ctx.send(emoji)
         await ctx.message.delete()
