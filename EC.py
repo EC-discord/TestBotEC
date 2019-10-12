047a90a41d8998a7c29b5eddc838b26ed0d046fa
EC <saeqaed@yahoo.com>
2019-10-12 20:13:05 +0500

    Update EC.py

--- EC.py
+++ EC.py
@@ -100,12 +100,14 @@ class jakeBot(commands.Bot):
                         self.spam[m.author.id]["frequency"] += 1
                     if m.content != self.spam[m.author.id]["message"]:
                         self.spam[m.author.id]["frequency"] = 1
-                if self.spam[m.author.id]["frequency"] == 5:
-                    await m.channel.send("**Warning**\nyou're spamming the same message over and over(**frequency: {self.spam[m.author.id]["frequency"]}**), if this frequency reaches **7** you will be kicked")
                 if self.spam[m.author.id]["frequency"] >= 7:
                     self.spam[m.author.id]["frequency"] = 0
                     await m.author.kick()
                     await m.channel.send(f"{m.author.name} has been kicked for spamming")
+                if self.spam[m.author.id]["frequency"] >= 5:
+                    await m.channel.send(f"""**Warning**
+                    you're spamming the same message over and over(**frequency: {self.spam[m.author.id][\"frequency\"]}**\)
+                    if this frequency reaches **7** you will be kicked""")
             elif not m.author.bot:
                 self.spam[m.author.id] = {"message": m.content, "frequency": 1, "channel": m.channel}
         await self.process_commands(m)
