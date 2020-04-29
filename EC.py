28a20cc5950afd42b46b322e1197108f96722ef7
EC <saeqaed@yahoo.com>
2020-04-29 12:14:39 +0500

    Update EC.py

--- EC.py
+++ EC.py
@@ -84,15 +84,6 @@ class jakeBot(commands.Bot):
         await self.invoke(ctx)
         
     async def on_message(self, m):
-        list = ["fuck", "fck", "hoe", "bitch", "bitxh", "cunt", "pussy", "dick", "asshole", "@sshole", "4sshole", 
-                "slut", "nigger", "faggot", "fucking", "fking", "faking", "fak", "whore", "fag", "nigga", "motherfucker",
-                "puta", "nygga", "cyka", "fucker", "fuk", "dickhead", "p0rn", "hentai", "h3ntai", "porn", "h3nta1", 
-                "f*ck", "blyat", "pp", "pron", "fuckin", "fukin", "fucking", "dik"]
-        if m.embeds:
-          l = m.embeds[0].description.lower().split()
-          for word in l:
-            if word in list:    
-              await m.delete()
         await self.process_commands(m)
       
     async def on_message_edit(self, before, after):
